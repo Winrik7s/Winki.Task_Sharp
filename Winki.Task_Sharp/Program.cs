6fa85e5a8a11b66c3f06e57af80422f63005bb4b
winrikls <winrikls@gmail.com>
2024-09-19 16:19:15 +0300

    Completed the task of comparing the areas of figures.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -2,40 +2,38 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Foo(float a, float b, out float resultC, out float resultS)
         {
-            short year;
-            short month;
+            resultC = (float)(3.14 * (a * a));
+            resultS = (float)(b * b);
 
-            short yearH;
-            short monthH;
-
-            short result;
-
-            Console.Write("Enter your year of birth: ");
-            yearH = Convert.ToInt16(Console.ReadLine());
-
-            Console.Write("Enter your month of birth: ");
-            monthH = Convert.ToInt16(Console.ReadLine());
-
-            Console.WriteLine();
-
-            Console.Write("Enter current year: ");
-            year = Convert.ToInt16(Console.ReadLine());
-
-            Console.Write("Enter current month: ");
-            month = Convert.ToInt16(Console.ReadLine());
-
-            result = (short)(year - yearH);
-
-            if(month == monthH)
+            if (resultC > resultS)
             {
-                Console.WriteLine($"You're {result} years old!");
+                Console.WriteLine("The square fit in a circle!");
             }
             else
             {
-                Console.WriteLine($"You're {result} years and {month - monthH} month old!");
+                Console.WriteLine("The circle fit in the square!");
             }
+        }
+
+        static void Main(string[] args)
+        {
+            float r;
+            float a;
+
+            float A;
+            float S;
+
+            Console.Write("Enter radius circle: ");
+            r = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Enter side square: ");
+            a = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Foo(r, a, out A, out S);
 
         }
     }
