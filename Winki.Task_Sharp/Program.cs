cfe3f6cb46afe56acefea59238d99efd98abe2f9
winrikls <winrikls@gmail.com>
2024-09-19 15:46:11 +0300

    Completed the task of calculating the user's age.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            
+            short year;
+            short month;
+
+            short yearH;
+            short monthH;
+
+            short result;
+
+            Console.Write("Enter your year of birth: ");
+            yearH = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Enter your month of birth: ");
+            monthH = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Enter current year: ");
+            year = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Enter current month: ");
+            month = Convert.ToInt16(Console.ReadLine());
+
+            result = (short)(year - yearH);
+
+            if(month == monthH)
+            {
+                Console.WriteLine($"You're {result} years old!");
+            }
+            else
+            {
+                Console.WriteLine($"You're {result} years and {month - monthH} month old!");
+            }
+
         }
     }
 }
