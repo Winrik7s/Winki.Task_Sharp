407810494e62fe2fc5cb1d255363f3ec2c13fb28
winrikls <winrikls@gmail.com>
2024-09-27 17:35:22 +0300

    Changed the conditions of the task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -2,18 +2,18 @@
 {
     public class Program
     {
-        static void Foo(float a, float b, out float resultC, out float resultS)
+        static void Foo(float a, float b, float c, out float resultC, out float resultT)
         {
             resultC = (float)(3.14 * (a * a));
-            resultS = (float)(b * b);
+            resultT = a * c / 2;
 
-            if (resultC > resultS)
+            if (resultC > resultT)
             {
-                Console.WriteLine("The square fit in a circle!");
+                Console.WriteLine("The triangle fit in a circle!");
             }
             else
             {
-                Console.WriteLine("The circle fit in the square!");
+                Console.WriteLine("The circle fit in the triangle!");
             }
         }
 
@@ -22,18 +22,23 @@
             float r;
             float a;
 
+            float h;
+
             float A;
             float S;
 
             Console.Write("Enter radius circle: ");
             r = Convert.ToSingle(Console.ReadLine());
 
-            Console.Write("Enter side square: ");
+            Console.Write("Enter the length of the side of the triangle: ");
             a = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Enter the length of the triangle height: ");
+            h = Convert.ToSingle(Console.ReadLine());
 
             Console.WriteLine();
 
-            Foo(r, a, out A, out S);
+            Foo(r, a, h, out A, out S);
 
         }
     }
