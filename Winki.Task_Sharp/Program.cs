a181fb7a516c21896a0784eac6d3a6fb83cad3cd
winrikls <winrikls@gmail.com>
2024-09-28 16:11:23 +0300

    Completed the assignment about coordinates.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,27 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine("Enter the coordinates of the lower left corner of the first rectangle (x1, y1):");
 
             short x1 = Convert.ToInt16(Console.ReadLine());
             short y1 = Convert.ToInt16(Console.ReadLine());
 
-            Console.WriteLine("");
+            Console.WriteLine("Enter the width and height of the first rectangle:");
 
             float w1 = Convert.ToSingle(Console.ReadLine());
             float h1 = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("");
+            Console.WriteLine("Enter the coordinates of the lower left corner of the second rectangle (x1, y1):");
 
             short x2 = Convert.ToInt16(Console.ReadLine());
             short y2 = Convert.ToInt16(Console.ReadLine());
 
-            Console.WriteLine("");
+            Console.WriteLine("Enter the width and height of the second rectangle:");
 
             float w2 = Convert.ToSingle(Console.ReadLine());
             float h2 = Convert.ToSingle(Console.ReadLine());
 
+            float x1Max = x1 + w1;
+            float y1Max = y1 + h1;
 
+            float x2Max = x2 + w2;
+            float y2Max = y2 + h2;
+
+            float minX = Math.Min(x1, x2);
+            float minY = Math.Min(y1, y2);
+
+            float maxX = Math.Max(x1Max, x2Max);
+            float maxY = Math.Max(y1Max, y2Max);
+
+            Console.WriteLine($"Bottom left corner: {minX}, {minY}");
+            Console.WriteLine($"Upper right corner: {maxX}, {maxY}");
         }
     }
 }
