224846b7b0566df52d4d546dfae16af8558561be
winrikls <winrikls@gmail.com>
2024-09-28 15:48:06 +0300

    Initialized lost variables.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("");
+
+            short x1 = Convert.ToInt16(Console.ReadLine());
+            short y1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float w1 = Convert.ToSingle(Console.ReadLine());
+            float h1 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            short x2 = Convert.ToInt16(Console.ReadLine());
+            short y2 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float w2 = Convert.ToSingle(Console.ReadLine());
+            float h2 = Convert.ToSingle(Console.ReadLine());
+
+
         }
     }
 }
