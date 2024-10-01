51e612e241d44c67350990df7e9bbf6d12435fe1
winrikls <winrikls@gmail.com>
2024-10-01 18:37:29 +0300

    Fulfilled the second condition.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,22 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("");
+            Console.Write("Bishop move a: ");
             byte a = Convert.ToByte(Console.ReadLine());
 
-            Console.Write("");
+            Console.Write("Bishop move b: ");
             byte b = Convert.ToByte(Console.ReadLine());
 
-            const byte c = 1;
-            const byte d = 5;
+            const byte c = 6;
+            const byte d = 2;
 
-            if(a == 8 && b == 4) 
+            if((a == 6 && b == 4) && (c == 6 && d == 2))
             {
-                Console.WriteLine("");
+                Console.WriteLine("The bishop threatens the field c,d!");
             }
             else
             {
-                Console.WriteLine("");
+                Console.WriteLine("The elephant doesn't threaten the fields.");
             }
         }
     }
