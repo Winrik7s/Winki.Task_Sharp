59c65e11551fb945110a3fd4f23b66c03b32897a
winrikls <winrikls@gmail.com>
2024-10-01 18:15:19 +0300

    Added variables for fields.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("");
+            byte a = Convert.ToByte(Console.ReadLine());
+
+            Console.Write("");
+            byte b = Convert.ToByte(Console.ReadLine());
+
+            const byte c = 1;
+            const byte d = 5;
+
+            if(a == 8 && b == 4) 
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
         }
     }
 }
