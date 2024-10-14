cb2de06816e0496b8b3e285b691dbbd0ff0655bd
winrikls <winrikls@gmail.com>
2024-10-14 15:30:53 +0300

    Completed the task on division.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter m: ");
+            short m = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Enter n: ");
+            short n = Convert.ToInt16(Console.ReadLine());
+
+            if (m % n == 0) 
+            {
+                short result = (short)(m / n);
+                Console.WriteLine($"Result: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Indivisible!");
+            }
         }
     }
 }
