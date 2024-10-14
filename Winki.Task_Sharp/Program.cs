def82e0a88973cf313f76a7f2908384789d34cea
winrikls <winrikls@gmail.com>
2024-10-14 15:18:40 +0300

    Deleted the last task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,23 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Queen's move a: ");
-            byte a = Convert.ToByte(Console.ReadLine());
-
-            Console.Write("Queen's move b: ");
-            byte b = Convert.ToByte(Console.ReadLine());
-
-            const byte c = 1;
-            const byte d = 1;
-
-            if((a == 1 && b == 4) && (c == 1 && d == 1))
-            {
-                Console.WriteLine("The queen threatens the field c,d!");
-            }
-            else
-            {
-                Console.WriteLine("The queen does not threaten the pieces.");
-            }
+            
         }
     }
 }
