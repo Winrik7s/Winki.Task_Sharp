6317208cdd1560d003e997bcdb9fc156ecacd16b
winrikls <winrikls@gmail.com>
2024-10-22 16:26:13 +0300

    Cleared the field in the editor.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,21 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter b: ");
-            short b = Convert.ToInt16(Console.ReadLine());
-
-            Console.Write("Enter a: ");
-            short a = Convert.ToInt16(Console.ReadLine());
-
-            if (b % a == 0) 
-            {
-                short result = (short)(b / a);
-                Console.WriteLine($"A is a divisor of B: {result}");
-            }
-            else
-            {
-                Console.WriteLine("Indivisible!");
-            }
+            
         }
     }
 }
