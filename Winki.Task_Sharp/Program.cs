9a239a666a2a81d718ad1df13b8ef45c484284c8
winrikls <winrikls@gmail.com>
2024-10-22 17:01:24 +0300

    Minor changes.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -7,11 +7,18 @@
             Console.Write("Enter a: ");
             short a = Convert.ToInt16(Console.ReadLine());
 
-            bool resultE = a % 2 == 0 ? true : false;
-            bool resultS = a % 10 == 7 ? true : false;
+            if (a >= 100)
+            {
+                Console.WriteLine("The number most not exceed 100!");
+            }
+            else
+            {
+                bool resultE = a % 2 == 0 ? true : false;
+                bool resultS = a % 10 == 7 ? true : false;
 
-            Console.WriteLine($"Is number {a} even or odd? {resultE}");
-            Console.WriteLine($"Does the number {a} end with the number 7? {resultS}");
+                Console.WriteLine($"Is number {a} even or odd? Answer: {resultE}");
+                Console.WriteLine($"Does the number {a} end with the number 7? Answer: {resultS}");
+            }
         }
     }
 }
