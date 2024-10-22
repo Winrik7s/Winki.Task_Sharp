80550a2d244f7745d272295c9b423073d4dd85a5
winrikls <winrikls@gmail.com>
2024-10-22 17:03:38 +0300

    Cleared the editor field.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,21 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a: ");
-            short a = Convert.ToInt16(Console.ReadLine());
-
-            if (a >= 100)
-            {
-                Console.WriteLine("The number most not exceed 100!");
-            }
-            else
-            {
-                bool resultE = a % 2 == 0 ? true : false;
-                bool resultS = a % 10 == 7 ? true : false;
-
-                Console.WriteLine($"Is number {a} even or odd? Answer: {resultE}");
-                Console.WriteLine($"Does the number {a} end with the number 7? Answer: {resultS}");
-            }
+            
         }
     }
 }
