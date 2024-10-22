62ab6fd8bbdb3ffdc6398d2fca62de1d9d267e7b
winrikls <winrikls@gmail.com>
2024-10-22 17:14:04 +0300

    Sketched out the body of the program.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter a: ");
+            short a = Convert.ToInt16(Console.ReadLine());
+
+            short num1 = (short)(a / 10);
+            short num2 = (short)(a % 10);
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"");
+            }
+            else
+            {
+                Console.WriteLine($"");
+            }
+
+            if (num1 == num2)
+            {
+                Console.WriteLine($"");
+            }
+            else
+            {
+                Console.WriteLine($"");
+            }
+
         }
     }
 }
