0a206091f2b9917575726feaa1255b8de3c89518
winrikls <winrikls@gmail.com>
2024-10-22 16:35:34 +0300

    Completed a task with a ternary operator.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter a: ");
+            short a = Convert.ToInt16(Console.ReadLine());
+
+            bool resultE = a % 2 == 0 ? true : false;
+            bool resultS = a % 10 == 7 ? true : false;
+
+
         }
     }
 }
