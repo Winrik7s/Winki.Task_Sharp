612d67add5dc3464e787ce9c4740a4125a65c439
winrikls <winrikls@gmail.com>
2024-10-24 15:03:58 +0300

    Cube function realized.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -2,8 +2,31 @@
 {
     public class Program
     {
+        static void Cube(ref short x, out short result)
+        {
+            result = x;
+
+            short number1 = (short)(x / 10);
+            short number2 = (short)(x % 10);
+
+            short cub = (short)((short)(number1 * number1 * number1) + (number2 * number2 * number2) * 4);
+            short sqr = (short)(x * x);
+
+            if (cub == sqr)
+            {
+                Console.WriteLine($"For the number {result} - positive!");
+            }
+            else
+            {
+                Console.WriteLine($"For the number {result} - negative!");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("");
+            short number = Convert.ToInt16(Console.ReadLine());
+
             
         }
     }
