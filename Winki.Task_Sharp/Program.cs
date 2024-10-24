e96421bf1787685e84bf51d9f8e302ffc6774774
winrikls <winrikls@gmail.com>
2024-10-24 14:44:23 +0300

    Cleared the field in the editor for the first task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,29 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a: ");
-            short a = Convert.ToInt16(Console.ReadLine());
-
-            short num1 = (short)(a / 10);
-            short num2 = (short)(a % 10);
-
-            if (num1 == num2)
-            {
-                Console.WriteLine($"Numbers {num1} and {num2} are the same");
-            }
-            else
-            {
-                Console.WriteLine($"Numbers {num1} and {num2} are not the same");
-            }
-
-            if (num1 > num2)
-            {
-                Console.WriteLine("The first number is greater than the second");
-            }
-            else
-            {
-                Console.WriteLine("The second number is greater than the first");
-            }
+            
         }
     }
 }
