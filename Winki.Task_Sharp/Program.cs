1e158fa9b2f0ab955900a82de44f93fb133bdde7
winrikls <winrikls@gmail.com>
2024-10-24 15:29:48 +0300

    Another function is implemented, but for the second task already.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -2,9 +2,38 @@
 {
     public class Program
     {
+        static void Foo(ref short x, out short result)
+        {
+            short number1 = (short)(x / 10);
+            short number2 = (short)(x % 10);
+
+            result = (short)(number1 + number2);
+
+            if(result > 10)
+            {
+                Console.WriteLine($"The number {result} is two - digit!");
+
+                if(result >= x)
+                {
+                    Console.WriteLine($"The number {result} is greater than the number {x}");
+                }
+                else
+                {
+                    Console.WriteLine($"The number {result} is less than the number {x}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The number {result} is not two - digit!");
+            }
+        }
+
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter number: ");
+            short number = Convert.ToInt16(Console.ReadLine());
+
+
         }
     }
 }
