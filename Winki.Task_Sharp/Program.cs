c575f0ce0857d2fc1c2377969d588c0c409437c4
winrikls <winrikls@gmail.com>
2024-11-01 17:31:32 +0300

    The task for the multiplicity of three has been completed.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter number n: ");
+            short n = Convert.ToInt16(Console.ReadLine());
+
+            if (n < 10)
+            {
+                Console.WriteLine("Error! Enter a two-digit number.");
+            }
+            else
+            {
+                n = (short)((n / 10) + (n % 10));
+                
+                if(n % 3 == 0)
+                {
+                    Console.WriteLine($"The number {n} is a multiple of 3");
+                }
+                else
+                {
+                    Console.WriteLine($"The number {n} is not a multiple of 3");
+                }
+            }
         }
     }
 }
