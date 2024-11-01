64ca842b46f336227fe271477193d850d3663308
winrikls <winrikls@gmail.com>
2024-11-01 17:44:22 +0300

    Clearing the field for the second task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,28 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter number n: ");
-            short n = Convert.ToInt16(Console.ReadLine());
-
-            short a = 5;
-
-            if (n < 10)
-            {
-                Console.WriteLine("Error! Enter a two-digit number.");
-            }
-            else
-            {
-                n = (short)((n / 10) + (n % 10));
-                
-                if(n % a == 0)
-                {
-                    Console.WriteLine($"The number {n} is a multiple of {a}");
-                }
-                else
-                {
-                    Console.WriteLine($"The number {n} is not a multiple of {a}");
-                }
-            }
+            
         }
     }
 }
