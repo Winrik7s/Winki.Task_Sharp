70e46a7f6f99ee893e5b49a48760d883aa76f909
winrikls <winrikls@gmail.com>
2024-11-01 17:57:36 +0300

    Implementation of the polyndrome check function.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -2,9 +2,27 @@
 {
     public class Program
     {
+        static void Foo(ref short x, out short result)
+        {
+            short x1 = (short)((x / 100));
+            short x2 = (short)(((x / 10) % 10));
+            short x3 = (short)(((x % 100) % 10));
+
+            result = (short)((x3 * 100) + (x2 * 10) + (x1));
+
+            if(result == x)
+            {
+                Console.WriteLine($"The number {result} is a polyndrome!");
+            }
+            else
+            {
+                Console.WriteLine($"The number {result} is not a polyndrome!");
+            }
+        }
+
         static void Main(string[] args)
         {
-            
+            Console.Write("");
         }
     }
 }
