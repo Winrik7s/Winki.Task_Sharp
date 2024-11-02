c7b83077da86d0d4df46338765847d402f7dd090
winrikls <winrikls@gmail.com>
2024-11-02 13:51:36 +0300

    Dividing a number into individual digits.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter three-digit number: ");
+            short number = Int16.Parse(Console.ReadLine());
+
+            short num1 = (short)(number / 100);
+            short num2 = (short)((number / 10) % 10);
+            short num3 = (short)(number % 10);
         }
     }
 }
