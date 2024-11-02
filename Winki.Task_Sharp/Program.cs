8793964afa2c75aa9feee4d134b23ffd2abb4020
winrikls <winrikls@gmail.com>
2024-11-02 14:19:59 +0300

    Var for math calc defined.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter a three-digit number: ");
+            short number = short.Parse(Console.ReadLine());
+
+            short num1 = (short)(number / 100);
+            short num2 = (short)((number / 10) % 10);
+            short num3 = (short)(number % 10);
+
+            short nSqr = (short)(number * number);
+            short sum = (short)((num1 * num1 * num1) + (num2 * num2 * num2) + (num3 * num3 * num3));
+
+
         }
     }
 }
