3f2a4161c4167d281f47b15a79695066cbfc939b
winrikls <winrikls@gmail.com>
2024-11-02 14:33:42 +0300

    Second task completed.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -7,20 +7,27 @@
             Console.Write("Enter a three-digit number: ");
             short number = short.Parse(Console.ReadLine());
 
-            short num1 = (short)(number / 100);
-            short num2 = (short)((number / 10) % 10);
-            short num3 = (short)(number % 10);
-
-            short nSqr = (short)(number * number);
-            short sum = (short)((num1 * num1 * num1) + (num2 * num2 * num2) + (num3 * num3 * num3));
-
-            if(nSqr >= sum)
+            if (number < 100 || number > 999)
             {
-                Console.WriteLine($"The square ({nSqr}) is greater than the sum of the cubes");
+                Console.WriteLine("Error!");
             }
             else
             {
-                Console.WriteLine($"The sum of the cubes ({sum}) is greater than the square of the number");
+                short num1 = (short)(number / 100);
+                short num2 = (short)((number / 10) % 10);
+                short num3 = (short)(number % 10);
+
+                short nSqr = (short)(number * number);
+                short sum = (short)((num1 * num1 * num1) + (num2 * num2 * num2) + (num3 * num3 * num3));
+
+                if (nSqr >= sum)
+                {
+                    Console.WriteLine($"The square ({nSqr}) is greater than the sum of the cubes");
+                }
+                else
+                {
+                    Console.WriteLine($"The sum of the cubes ({sum}) is greater than the square of the number");
+                }
             }
         }
     }
