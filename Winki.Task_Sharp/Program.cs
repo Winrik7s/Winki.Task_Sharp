ffaa10bc1c6255437fd7dd6d0d0ed2c4f1b86a16
winrikls <winrikls@gmail.com>
2024-11-03 17:18:57 +0300

    The first condition of the task is completed.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter a three-digit number: ");
+            short number = Convert.ToInt16(Console.ReadLine());
+
+            if(number < 100 || number > 999)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                short sum = (short)((number / 100) + (number / 10 % 10) + (number % 10));
+
+                if (sum >= 10)
+                {
+                    Console.WriteLine($"The sum of digits of the number {number} is two digit({sum})");
+                }
+                else
+                {
+                    Console.WriteLine($"The sum of digits of the number {number} is not a two digit({sum})");
+                }
+            }
         }
     }
 }
