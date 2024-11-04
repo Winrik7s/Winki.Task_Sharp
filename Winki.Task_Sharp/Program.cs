6906f45ceb7058db1cb121294a7145be0587d857
winrikls <winrikls@gmail.com>
2024-11-04 16:59:24 +0300

    The text field has been cleared.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,26 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a three-digit number: ");
-            short number = Convert.ToInt16(Console.ReadLine());
-
-            if(number < 100 || number > 999)
-            {
-                Console.WriteLine("Error!");
-            }
-            else
-            {
-                short sum = (short)((number / 100) + (number / 10 % 10) + (number % 10));
-
-                if (sum % 3 == 0)
-                {
-                    Console.WriteLine($"The sum of digits of {number} is a multiple 3");
-                }
-                else
-                {
-                    Console.WriteLine($"The sum of digits of {number} is not a multiple 3");
-                }
-            }
+            
         }
     }
 }
