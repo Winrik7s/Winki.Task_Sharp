794636b08379ffdf531cadf54a40a7c58d2f6b66
winrikls <winrikls@gmail.com>
2024-11-04 18:07:04 +0300

    Comleted the same task, but with added language selection - Ru.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,26 +4,51 @@
     {
         static void Main(string[] args)
         {
-            char[] Ensymbol = { 'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y'};
-            char[] Rusymbol = { 'А', 'а', 'У', 'у', 'О', 'о', 'И', 'и', 'Э', 'Ы', 'ы', 'Я', 'я', 'Ю', 'ю', 'Е', 'е', 'Ё', 'ё', 'э'};
-
             short count = 0;
+            Console.Write("Select the language of the letters(r - Rus, e - Eng): ");
 
-            Console.Write("Enter a string: ");
-            string? line = Console.ReadLine();
+            string? ch = Console.ReadLine().ToLower();
 
-            for (int i = 0; i < line?.Length; i++)
+            if(ch == "e")
             {
-                for(int j = 0; j < Ensymbol.Length; j++)
+                Console.Write("Enter a string: ");
+                string? line = Console.ReadLine();
+
+                char[] Ensymbol = { 'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y' };
+
+                for (int i = 0; i < line?.Length; i++)
                 {
-                    if (Ensymbol[i] == line[j])
+                    for (int j = 0; j < Ensymbol.Length; j++)
                     {
-                        count++;
+                        if (Ensymbol[j] == line[i])
+                        {
+                            count++;
+                        }
                     }
                 }
-            }
 
-            Console.WriteLine($"The number of vowel letters in the string {line} equals: {count}");
+                Console.WriteLine($"The number of vowel letters in the string {line} equals: {count}");
+            }
+            else if(ch == "r")
+            {
+                Console.Write("Введите строку: ");
+                string? line = Console.ReadLine();
+
+                char[] Rusymbol = { 'А', 'а', 'У', 'у', 'О', 'о', 'И', 'и', 'Э', 'Ы', 'ы', 'Я', 'я', 'Ю', 'ю', 'Е', 'е', 'Ё', 'ё', 'э' };
+
+                for (int i = 0; i < line?.Length; i++)
+                {
+                    for (int j = 0; j < Rusymbol.Length; j++)
+                    {
+                        if (Rusymbol[j] == line[i])
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                Console.WriteLine($"Кол - во гласных в строке {line} равняется: {count}");
+            }
         }
     }
 }
