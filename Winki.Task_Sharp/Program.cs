2e1f15e45abd2342d823e43a1332d0ed57c5955c
winrikls <winrikls@gmail.com>
2024-11-04 17:33:29 +0300

    Vowel search task completed.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            char[] symbol = { 'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y'};
+
+            short count = 0;
+
+            Console.Write("Enter a string: ");
+            string? line = Console.ReadLine();
+
+            for (int i = 0; i < line?.Length; i++)
+            {
+                for(int j = 0; j < symbol.Length; j++)
+                {
+                    if (symbol[i] == line[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"The number of vowel letters in the string {line} equals: {count}");
         }
     }
 }
