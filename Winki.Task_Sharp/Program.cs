6156d190318bdb456e705b5713b4d54006092de8
winrikls <winrikls@gmail.com>
2024-11-07 20:48:24 +0300

    Employee withdrawal task completed.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Winki.Task_Sharp
+﻿using System.Linq;
+
+namespace Winki.Task_Sharp
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            List<Staff> stf = new() { new Staff("Artem", "Roofer", 1600), new Staff("Katya", "Teacher", 800), new Staff("Ivan", "Programmer", 2000)};
 
+            Console.WriteLine("Staff list:");
+            stf.Sort((s1, s2) => s1.Name.CompareTo(s2.Name));
+
+            foreach(Staff staff in stf)
+            {
+                Console.WriteLine($"Name: {staff.Name}, Position: {staff.Position}, Salary: {staff.Salary}");
+            }
         }
     }
 }
