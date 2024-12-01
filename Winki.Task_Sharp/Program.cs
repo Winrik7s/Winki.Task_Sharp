d2a1362765bd9d061262405473942ea1267134c1
winrikls <winrikls@gmail.com>
2024-12-01 17:24:07 +0300

    Coded the main block of the program.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -26,7 +26,39 @@ namespace Winki.Task_Sharp
 
         static void Main(string[] args)
         {
-            
+            double result;
+
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Select an arithmetic operation (+, -, *, /): ");
+            string? operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    result = Add(num1, num2);
+                break;
+
+                case "-":
+                    result = Add(num1, num2);
+                break;
+
+                case "*":
+                    result = Add(num1, num2);
+                break;
+
+                case "/":
+                    result = Add(num1, num2);
+                break;
+
+                default:
+                    Console.WriteLine("Error! Enter the operation from the presented list.");
+                break;
+            }
         }
     }
 }
