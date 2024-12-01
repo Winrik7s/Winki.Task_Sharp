b6eb7de8aa0fb36228d98c2ff49d8d1fff59571d
winrikls <winrikls@gmail.com>
2024-12-01 16:43:56 +0300

    Clearing the field from the old code.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -6,15 +6,7 @@ namespace Winki.Task_Sharp
     {
         static void Main(string[] args)
         {
-            List<Staff> stf = new() { new Staff("Artem", "Roofer", 1600), new Staff("Katya", "Teacher", 800), new Staff("Ivan", "Programmer", 2000)};
-
-            Console.WriteLine("Staff list:");
-            stf.Sort((s1, s2) => s1.Name.CompareTo(s2.Name));
-
-            foreach(Staff staff in stf)
-            {
-                Console.WriteLine($"Name: {staff.Name}, Position: {staff.Position}, Salary: {staff.Salary}");
-            }
+            
         }
     }
 }
