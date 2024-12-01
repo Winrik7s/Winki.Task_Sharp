31ff4132976c4c3b847749bacd848032324079a7
winrikls <winrikls@gmail.com>
2024-12-01 16:54:08 +0300

    Add basic operations.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,6 +4,26 @@ namespace Winki.Task_Sharp
 {
     public class Program
     {
+        static double Add(double a, double b) 
+        {
+            return a + b;
+        }
+
+        static double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double Div(double a, double b)
+        {
+            return a / b;
+        }
+
         static void Main(string[] args)
         {
             
