d14dbfef1ab3e3d392cf77c6acb8328f3f7094c4
winrikls <winrikls@gmail.com>
2024-12-11 18:40:42 +0300

    Manual array filling.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            const float N = (float)100.78;
+            float[] arr = { 23.4f, 1.5f, -79.54f, 21.2f, 7.12f, 11.45f, 54.15f, 0.2f, 9.78f, 2.9f};
+
 
         }
     }
