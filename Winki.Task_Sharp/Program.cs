7df1cf16818b2ce63c2fb432da583e07fd682295
winrikls <winrikls@gmail.com>
2024-12-11 19:00:50 +0300

    Adding a cycle to calculate the amount and check for output.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -7,7 +7,21 @@
             const float N = (float)100.78;
             float[] arr = { 23.4f, 1.5f, -79.54f, 21.2f, 7.12f, 11.45f, 54.15f, 0.2f, 9.78f, 2.9f};
 
+            float sum = 0f;
 
+            for(int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            if(sum > N)
+            {
+                Console.WriteLine($"The sum of the elements {sum} is greater than the number {N}");
+            }
+            else
+            {
+                Console.WriteLine($"The sum of the elements {sum} is less than the number {N}");
+            }
         }
     }
 }
