8e200ab459ee2645950ac797a3050efb98bf5997
winrikls <winrikls@gmail.com>
2024-12-11 19:40:20 +0300

    The second task has been completed.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+
             short[] arr = new short[5];
             Console.WriteLine("Enter the array elements:\n");
 
@@ -11,6 +13,25 @@
             {
                 Console.Write($"arr[{i}] = ");
                 arr[i] = Convert.ToInt16(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Enter number: ");
+            short n = Convert.ToInt16(Console.ReadLine());
+
+            for(int k = 0; k < arr.Length; k++)
+            {
+                sum += arr[k];
+            }
+
+            if(sum < n)
+            {
+                Console.WriteLine($"The sum of the elements: {sum} is less than the number: {n}");
+            }
+            else
+            {
+                Console.WriteLine($"The sum of the elements: {sum} is greater than the number: {n}");
             }
         }
     }
