098d2a66775637d095b82e2b8a40bcac30a6be10
winrikls <winrikls@gmail.com>
2024-12-22 16:13:16 +0300

    The first part of the task is complete.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //Вариант А:
+            int lastNum = number % 10;
+
+            if(lastNum % 2 == 0) 
+            {
+                Console.WriteLine($"Last digit of {number} is even");
+            }
+            else
+            {
+                Console.WriteLine($"Last digit of {number} is not even");
+            }
         }
     }
 }
