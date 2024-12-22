2a1893ac8158381cb4a12a43b2de94028b941ff3
winrikls <winrikls@gmail.com>
2024-12-22 16:20:14 +0300

    Clearing the field for the second task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,34 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter number: ");
-            int number = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine();
-
-            //Вариант А:
-            int lastNum = number % 10;
-
-            if(lastNum % 2 == 0) 
-            {
-                Console.WriteLine($"Last digit of {number} is even");
-            }
-            else
-            {
-                Console.WriteLine($"Last digit of {number} is not even");
-            }
-
-            Console.WriteLine();
-
-            //Вариант Б:
-            if (lastNum % 2 != 0)
-            {
-                Console.WriteLine($"Last digit of is not even");
-            }
-            else
-            {
-                Console.WriteLine($"Last digit of is even");
-            }
+            
         }
     }
 }
