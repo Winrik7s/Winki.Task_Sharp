3d6a05d026e15887c6307a89522484af16cdaedd
winrikls <winrikls@gmail.com>
2024-12-24 17:49:37 +0300

    Drawing MessageBox.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,6 +4,9 @@ namespace Winki.Task_Sharp
 {
     public class Program
     {
+        [DllImport("user32.dll")]
+        static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+
         static void Main(string[] args)
         {
             
