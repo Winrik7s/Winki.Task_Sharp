593fac01ba118f756414f344f80d37a61cce8ddb
winrikls <winrikls@gmail.com>
2024-12-28 17:51:58 +0300

    Clearing the field for a task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -1,22 +1,10 @@
-﻿using System.Runtime.InteropServices;
-using System.Diagnostics;
-using Windows.Win32;
-
-namespace Winki.Task_Sharp
+﻿namespace Winki.Task_Sharp
 {
     public class Program
     {
-        [DllImport("user32.dll")]
-        static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
-
         static void Main(string[] args)
         {
-            MessageBox(IntPtr.Zero, "Hello, GitHub!", "Test Platform Invoke", 0);
 
-            var windowHandle = Process.GetCurrentProcess().MainWindowHandle;
-
-            PInvoke.MessageBox((Windows.Win32.Foundation.HWND)windowHandle, "Hello, GitHub!", 
-                "Test win32metadata", Windows.Win32.UI.WindowsAndMessaging.MESSAGEBOX_STYLE.MB_OK);
         }
     }
 }
