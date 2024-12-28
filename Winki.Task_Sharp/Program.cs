68262c639b4a1f5a8cb1ebc59011a2332e157352
winrikls <winrikls@gmail.com>
2024-12-28 18:31:11 +0300

    Added method to search for a digit in the entered number.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -2,6 +2,25 @@
 {
     public class Program
     {
+        static void F(short x)
+        {
+            short num1 = (short)(x / 10);
+            short num2 = (short)(x % 10);
+
+            if(num1 == 4 || num1 == 7) 
+            {
+                Console.WriteLine($"В введенное число входит цифра: {num1}");
+            }
+            else if (num2 == 4 || num2 == 7) 
+            {
+                Console.WriteLine($"В введенное число входит цифра: {num2}");
+            }
+            else
+            {
+                Console.WriteLine("В введенное число не входят цифры: 4, 7");
+            }
+        }
+
         static void Main(string[] args)
         {
 
