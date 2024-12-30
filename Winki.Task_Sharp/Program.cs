e3ea0120ec2595bef1d1b4bd38172c6c18bfe8ae
winrikls <winrikls@gmail.com>
2024-12-30 16:23:35 +0300

    The first part of the task has been completed.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if(number <= 0)
+            {
+                Console.WriteLine("The number must not be less than or equal to 0!");
+            }
+            else
+            {
+                int i = 10;
+
+                while (i < number)
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
+            }
         }
     }
 }
