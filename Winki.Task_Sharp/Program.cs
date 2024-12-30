bb49346c8277cf004129614c737242b5970ee5a0
winrikls <winrikls@gmail.com>
2024-12-30 16:08:38 +0300

    Clearing the field of past tasks.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -2,38 +2,9 @@
 {
     public class Program
     {
-        static void F(short x)
-        {
-            short num1 = (short)(x / 10);
-            short num2 = (short)(x % 10);
-
-            if(num1 == 3 || num1 == 6 || num1 == 9) 
-            {
-                Console.WriteLine($"В введенное число входит цифра: {num1}");
-            }
-            else if (num2 == 3 || num2 == 6 || num2 == 9) 
-            {
-                Console.WriteLine($"В введенное число входит цифра: {num2}");
-            }
-            else
-            {
-                Console.WriteLine("В введенное число не входят цифры: 3, 6, 9");
-            }
-        }
-
         static void Main(string[] args)
         {
-            Console.Write("Введите число: ");
-            short number = Convert.ToInt16(Console.ReadLine());
-
-            if(number < 10 || number > 99) 
-            {
-                Console.WriteLine("Число должно быть двузначным!");
-            }
-            else
-            {
-                F(number);
-            }
+            
         }
     }
 }
