6920a7eac69439f76d74ce2c7af7b9a4e0810229
winrikls <winrikls@gmail.com>
2025-01-17 17:58:21 +0300

    Clearing the field from the previous task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -2,32 +2,8 @@
 {
     public class Program
     {
-        static void F(int number)
-        {
-            if(number <= 0)
-            {
-                Console.WriteLine("The number is not a positive integer!");
-            }
-            else
-            {
-                if (number % 2 != 0)
-                {
-                    Console.WriteLine("The number is prime");
-                }
-                else
-                {
-                    Console.WriteLine("The number is not prime");
-                }
-            }
-
-        }
-
         static void Main(string[] args)
         {
-            Console.Write("Type an integer: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-
-            F(num);
 
         }
     }
