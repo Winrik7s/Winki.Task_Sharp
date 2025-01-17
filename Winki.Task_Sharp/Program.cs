11f1ae97d0e4a0deb452859e2616b822a3ace421
winrikls <winrikls@gmail.com>
2025-01-17 18:41:42 +0300

    Completed the second task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -2,7 +2,7 @@
 {
     public class Program
     {
-        static decimal InterestOnTheDeposit(decimal sum, decimal deposit, short day)
+        static decimal InterestOnTheDeposit(decimal sum, short deposit, short day)
         {
             decimal dep = sum * deposit * day;
             return dep;
@@ -10,7 +10,23 @@
 
         static void Main(string[] args)
         {
-            
+            const short day = 120;
+
+            Console.Write("Enter the deposit amount: ");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+
+            if(sum < 100)
+            {
+                Console.WriteLine($"Deposit amount with accrued interest: {InterestOnTheDeposit(sum, 5, day)}");
+            }
+            else if(sum >= 100 || sum <= 200)
+            {
+                Console.WriteLine($"Deposit amount with accrued interest: {InterestOnTheDeposit(sum, 7, day)}");
+            }
+            else if(sum > 200)
+            {
+                Console.WriteLine($"Deposit amount with accrued interest: {InterestOnTheDeposit(sum, 10, day)}");
+            }
         }
     }
 }
