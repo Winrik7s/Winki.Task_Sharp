728c38ac7f2ef8e9c23a979f2fa14dedaf05a744
winrikls <winrikls@gmail.com>
2025-01-17 18:14:13 +0300

    Solution of the second task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,17 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter number: ");
-            byte num = Convert.ToByte(Console.ReadLine());
-
-            if(num == 5 || num == 10)
-            {
-                Console.WriteLine("The number is equal to 5 or 10");
-            }
-            else
-            {
-                Console.WriteLine("Unknown number!");
-            }
+            
         }
     }
 }
