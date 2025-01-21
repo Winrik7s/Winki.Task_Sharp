d54891abfdbf910ea371949b42925bafb210921c
winrikls <winrikls@gmail.com>
2025-01-21 17:46:08 +0300

    Completed the task with the loop.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter the deposit amount: ");
+            decimal dep = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Enter the number of months: ");
+            int mon = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i <= mon; i++)
+            {
+                dep += dep * 0.07M;
+            }
+
+            Console.WriteLine($"Final deposit amount: {dep}");
         }
     }
 }
