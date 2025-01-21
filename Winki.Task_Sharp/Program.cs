53b4ee2508e9cbc270b09df02531364ff96b404b
winrikls <winrikls@gmail.com>
2025-01-21 17:28:40 +0300

    Cleared the field from the previous task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -2,31 +2,9 @@
 {
     public class Program
     {
-        static decimal InterestOnTheDeposit(decimal sum, decimal deposit, short day)
-        {
-            decimal dep = sum * deposit * day;
-            return dep;
-        }
-
         static void Main(string[] args)
         {
-            const short day = 120;
-
-            Console.Write("Enter the deposit amount: ");
-            decimal sum = Convert.ToDecimal(Console.ReadLine());
-
-            if(sum < 100)
-            {
-                Console.WriteLine($"Deposit amount with accrued interest: {InterestOnTheDeposit(sum, 0.05M, day)}");
-            }
-            else if(sum >= 100 || sum <= 200)
-            {
-                Console.WriteLine($"Deposit amount with accrued interest: {InterestOnTheDeposit(sum, 0.07M, day)}");
-            }
-            else if(sum > 200)
-            {
-                Console.WriteLine($"Deposit amount with accrued interest: {InterestOnTheDeposit(sum, 0.1M, day)}");
-            }
+            
         }
     }
 }
