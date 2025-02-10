7433c1815c5682f915f00e6f114204ff5546ac6e
winrikls <winrikls@gmail.com>
2025-02-10 16:53:38 +0300

    Completed the task of outputting the multiplication table.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            for(int i = 1; i <= 10; i++)
+            {
+                for(int j = 1; j <= 10; j++)
+                {
+                    Console.Write($"{i} * {j} = {i * j}\n");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
