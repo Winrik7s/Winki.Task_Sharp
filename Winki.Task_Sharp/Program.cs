31ce96fd43d66c79671cecbe4f44edd83e58d3b1
winrikls <winrikls@gmail.com>
2025-02-10 16:32:04 +0300

    New task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,18 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the deposit amount: ");
-            decimal dep = Convert.ToDecimal(Console.ReadLine());
-
-            Console.Write("Enter the number of months: ");
-            int mon = Convert.ToInt32(Console.ReadLine());
-
-            for(int i = 1; i <= mon; i++)
-            {
-                dep += dep * 0.07M;
-            }
-
-            Console.WriteLine($"Final deposit amount: {dep}");
+            
         }
     }
 }
