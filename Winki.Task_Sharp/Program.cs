ebcd54e72fd9fc174f28830dc6b92bb5fd1eb30f
winrikls <winrikls@gmail.com>
2025-02-10 17:31:02 +0300

    Completed the multiplication task in a certain range.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
+            int num1, num2;
+
+            while (true)
+            {
+                num1 = Convert.ToInt32(Console.ReadLine());
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                if (num1 < 0 || num1 > 10 || num2 < 0 || num2 > 10)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"{num1 * num2}");
+                }
+            }
         }
     }
 }
