33885d024010040e08dc8de6c53a5acdb1783bae
winrikls <winrikls@gmail.com>
2025-04-06 15:12:52 +0300

    Cleared the field for the next task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,24 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the first number: ");
-            float num1 = Convert.ToSingle(Console.ReadLine());
-
-            Console.Write("Enter the second number: ");
-            float num2 = Convert.ToSingle(Console.ReadLine());
-
-            if(num1 == num2)
-            {
-                Console.WriteLine("The two numbers are equal");
-            }
-            else if(num1 > num2)
-            {
-                Console.WriteLine("The first number is greater than the second");
-            }
-            else if(num1 < num2)
-            {
-                Console.WriteLine("The first number is less than the second");
-            }
+            
         }
     }
 }
