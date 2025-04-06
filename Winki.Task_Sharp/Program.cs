d1d8603cd45da9a4be3a54b543069d7d47bd9ead
winrikls <winrikls@gmail.com>
2025-04-06 15:22:03 +0300

    Second task completed.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter a number: ");
+            float number = Convert.ToSingle(Console.ReadLine());
+
+            if(number > 5 && number < 10)
+            {
+                Console.WriteLine("A number greater than 5 and less than 10");
+            }
+            else
+            {
+                Console.WriteLine("Unknown number");
+            }
         }
     }
 }
