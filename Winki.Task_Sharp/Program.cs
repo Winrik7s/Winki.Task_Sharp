d9c21551cf958e7f40580b40ec3c06a7070f7744
winrikls <winrikls@gmail.com>
2025-04-07 20:41:49 +0300

    The second task has been completed.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter the deposit amount: ");
+
+            float percent;
+            float amount = Convert.ToSingle(Console.ReadLine());
+
+            if (amount < 100)
+            {
+                percent = (float)(amount * 0.05);
+            }
+            else if(amount >= 100)
+            {
+                percent = (float)(amount * 0.07);
+            }
+            else
+            {
+                percent = (float)(amount * 0.1);
+            }
+
+            Console.WriteLine($"Investment amount with interest: {percent + amount}");
         }
     }
 }
