f7bdf505850f8d02fc7aaeb6a77665e9fa7b6a8e
winrikls <winrikls@gmail.com>
2025-04-07 20:26:36 +0300

    First task completed.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter number: ");
+            byte number = Convert.ToByte(Console.ReadLine());
+
+            if (number == 5 || number == 10)
+            {
+                Console.WriteLine("The number is either exactly 5 or exactly 10");
+            }
+            else
+            {
+                Console.WriteLine("Unknown number");
+            }
         }
     }
 }
