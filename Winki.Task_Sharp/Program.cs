19c963db4be4d041157c4c6a640549f102ea7104
winrikls <winrikls@gmail.com>
2025-04-24 22:31:10 +0300

    Created a method for finding the final contribution amount.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -2,6 +2,16 @@
 {
     public class Program
     {
+        static decimal Amount(decimal deposit, short month)
+        {
+            for(int i = 1; i <= month; i++)
+            {
+                deposit += deposit * 0.07M;
+            }
+
+            return deposit;
+        }
+
         static void Main(string[] args)
         {
             
