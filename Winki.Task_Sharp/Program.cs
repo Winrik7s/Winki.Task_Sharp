2ce7b0aac8f0fb602082fb320083fc3145ea8081
winrikls <winrikls@gmail.com>
2025-04-25 20:10:54 +0300

    Cleared the text field.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -2,28 +2,9 @@
 {
     public class Program
     {
-        static decimal Amount(decimal deposit, short month)
-        {
-            int i = month;
-
-            while(i > 0)
-            {
-                deposit += deposit * 0.07M;
-                i--;
-            }
-
-            return deposit;
-        }
-
         static void Main(string[] args)
         {
-            Console.Write("Enter the deposit amount: ");
-            decimal deposit = Convert.ToDecimal(Console.ReadLine());
-
-            Console.Write("Enter number of months: ");
-            short month = Convert.ToInt16(Console.ReadLine());
-
-            Console.WriteLine($"Final deposit amount: {Amount(deposit, month)}");
+            
         }
     }
 }
