2bda0574421a6291851c1d53637a1324514a973c
winrikls <winrikls@gmail.com>
2025-04-25 20:35:34 +0300

    Cleared the first task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,15 +4,7 @@
     {
         static void Main(string[] args)
         {
-            for(int i = 1; i <= 10; i++)
-            {
-                for(int j = i; j <= 10; j++)
-                {
-                    Console.WriteLine($"{i * j}\t");
-                }
 
-                Console.WriteLine();
-            }
         }
     }
 }
