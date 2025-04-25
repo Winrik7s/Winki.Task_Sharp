b5595c2971a55f2cbef860454f96c1779dfc5959
winrikls <winrikls@gmail.com>
2025-04-25 20:57:22 +0300

    Finished the second task.

--- Winki.Task_Sharp/Program.cs
+++ Winki.Task_Sharp/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
+            float number1, number2;
 
+            float mul;
+
+            while(true)
+            {
+                Console.Write("Enter the first number: ");
+                number1 = Convert.ToSingle(Console.ReadLine());
+
+                Console.Write("Enter the second number: ");
+                number2 = Convert.ToSingle(Console.ReadLine());
+
+                if (number1 < 0 || number1 > 10 || number2 < 0 || number2 > 10)
+                {
+                    Console.WriteLine("Invalid values!");
+                }
+                else
+                {
+                    mul = (float)(number1 * number2);
+                    Console.WriteLine($"Multiplication: {mul}");
+                    break;
+                }
+            }
         }
     }
 }
